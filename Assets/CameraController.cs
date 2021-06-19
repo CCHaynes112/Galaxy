a75fed4f751fbf024b568e8af8a5f1f3ee8d1300
Curtis <CCHaynes1122@gmail.com>
2021-06-18 21:07:46 -0700

    Initial

---
+++ Assets/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 rotation = Vector2.zero;
+    public float lookSpeed = 3;
+    public float moveSpeed = 5;
+
+    void Update()
+    {
+        if (Input.GetKey("q"))
+        {
+            moveSpeed -= 5;
+        }
+        if (Input.GetKey("e"))
+        {
+            moveSpeed += 5;
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += -transform.right * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += -transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * Time.deltaTime * moveSpeed;
+        }
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * lookSpeed;
+    }
+}
---
+++ Assets/GenerateNodes.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateNodes : MonoBehaviour
+{
+    public GameObject star;
+    public GameObject planet;
+    public int galaxySize = 1500;
+    public int planetsPerSystem = 10;
+    public int solarSystemsPerCluster = 25;
+    public int clusterCount = 5; // How many solar system clusters we want
+    public int clusterSize = 700;
+    public int solarSystemSize = 50;
+    List<Vector3> systemClusters; // List of locations for each solar system cluster
+    List<Vector3> solarSystems; // List of locations for each solar system
+
+
+    void Start()
+    {
+        systemClusters = new List<Vector3>();
+        solarSystems = new List<Vector3>();
+        GenerateSystemClusters();
+        GenerateSolarSystems();
+        GenerateStars();
+    }
+
+    void GenerateSystemClusters()
+    {
+        for (int i = 0; i < clusterCount; i++)
+        {
+            float randomx = Random.Range(-galaxySize, galaxySize);
+            float randomy = Random.Range(-galaxySize, galaxySize);
+            float randomz = Random.Range(-galaxySize, galaxySize);
+            systemClusters.Add(new Vector3(randomx, randomy, randomz));
+        }
+    }
+
+    void GenerateSolarSystems()
+    {
+        for (int i = 0; i < systemClusters.Count; i++)
+        {
+            for (int j = 0; j < solarSystemsPerCluster; j++)
+            {
+                float randomx = Random.Range(-clusterSize, clusterSize);
+                float randomy = Random.Range(-clusterSize, clusterSize);
+                float randomz = Random.Range(-clusterSize, clusterSize);
+                solarSystems.Add(systemClusters[i] + new Vector3(randomx, randomy, randomz));
+            }
+        }
+    }
+
+    void GenerateStars()
+    {
+        for (int i = 0; i < solarSystems.Count; i++)
+        {
+            GameObject solarSystemParent = new GameObject("Solar System");
+            Instantiate(star, solarSystems[i], transform.rotation, solarSystemParent.transform);
+            for (int j = 0; j < planetsPerSystem; j++)
+            {
+                AddPlanet(solarSystems[i], solarSystemParent.transform);
+            }
+        }
+    }
+
+    void AddPlanet(Vector3 center, Transform parentTransform)
+    {
+        float randomx = Random.Range(-solarSystemSize, solarSystemSize);
+        float randomy = Random.Range(-solarSystemSize, solarSystemSize);
+        float randomz = Random.Range(-solarSystemSize, solarSystemSize);
+        Vector3 position = new Vector3(center.x + randomx, center.y + randomy, center.z + randomz);
+        Instantiate(planet, position, transform.rotation, parentTransform);
+    }
+}
